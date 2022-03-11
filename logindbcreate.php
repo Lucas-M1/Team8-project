e5da2571df66f4edff9c04260b2840b3e2effcd1
mjalal1 <54548628+mjalal1@users.noreply.github.com>
2022-03-11 07:20:13 +0000

    Add files via upload

--- logindbcreate.php
+++ logindbcreate.php
@@ -1,11 +1,20 @@
 <?php
-$mysqli = new mysqli("localhost", "root", "");
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'logininfo');
+
+$mysqli = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+
  
 // Check connection
+if($mysqli === true){
+    echo "Connection to SQL database established";
+}
 if($mysqli === false){
     die("ERROR: Could not connect. " . $mysqli->connect_error);
 }
-$sql = "CREATE DATABASE logininfo";
+$sql = "CREATE DATABASE if not exists logininfo";
 if($mysqli->query($sql) === true){
     echo "Database created successfully";
 } else{
