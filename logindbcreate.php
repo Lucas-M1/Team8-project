ad2baf2c8a74e511c26f81f2bfceaa0bb12bae9d
mjalal1 <54548628+mjalal1@users.noreply.github.com>
2022-03-17 21:37:23 +0000

    Update logindbcreate.php

--- logindbcreate.php
+++ logindbcreate.php
@@ -9,16 +9,18 @@ $mysqli = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
  
 // Check connection
 if($mysqli === true){
-    echo "Connection to SQL database established";
+    echo "";
 }
 if($mysqli === false){
     die("ERROR: Could not connect. " . $mysqli->connect_error);
 }
+
+//Creates database to store user's login info
 $sql = "CREATE DATABASE if not exists logininfo";
 if($mysqli->query($sql) === true){
     echo "";
 } else{
-    echo "ERROR: Was not able to execute $sql. " . $mysqli->error;
+    echo "ERROR: Could not able to execute $sql. " . $mysqli->error;
 }
  
 $mysqli->close();
